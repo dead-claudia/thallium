ab0ba1af5c7fd3db3fcff900f9220046480a0ced
impinball <impinball@gmail.com>
2016-07-07 03:35:22 -0400

    Add --single-run to karma tests on Travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -19,8 +19,8 @@ case ${ENV} in
         wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
         sudo dpkg -i google-chrome*.deb
         export CHROME_BIN=/usr/bin/google-chrome
-        karma start --browsers ChromeTravisCI
+        karma start --browsers ChromeTravisCI --single-run
         ;;
-    firefox*) karma start --browsers Firefox ;;
+    firefox*) karma start --browsers Firefox --single-run ;;
     *) echo "Unknown \${ENV}: ${ENV}"; exit 1 ;;
 esac
