785178b45e35272bccf2d8a0ed07368ba18da673
impinball <impinball@gmail.com>
2016-10-13 02:52:45 -0400

    Enable colors in CI

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,14 +2,14 @@
 set -ev
 
 case ${ENV} in
-    node) mocha ;;
+    node) mocha --colors ;;
     chrome)
         sudo apt-get update
         sudo apt-get install -y libappindicator1 fonts-liberation
         wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
         sudo dpkg -i google-chrome*.deb
         export CHROME_BIN=/usr/bin/google-chrome
-        karma start --browsers ChromeTravisCI --single-run
+        karma --colors start --browsers ChromeTravisCI --single-run
         ;;
     firefox*) karma start --browsers Firefox --single-run ;;
     *) echo "Unknown \${ENV}: ${ENV}"; exit 1 ;;
