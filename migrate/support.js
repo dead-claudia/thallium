ea230db41cb97a9f9f429d726f5e7276f56db492
Isiah Meadows <me@isiahmeadows.com>
2017-06-12 11:49:22 -0400

    Remove old artifact, add `call` to support

--- migrate/support.js
+++ migrate/support.js
@@ -1,1 +1,15 @@
 "use strict"
+
+/**
+ * Replaces the old `t.call` non-invasively.
+ */
+exports.call = function (t, plugin) {
+    var reflect = t.reflect
+    var args = [reflect]
+
+    for (var i = 2; i < arguments.length; i++) {
+        args.push(arguments[i])
+    }
+
+    return plugin.apply(reflect, args)
+}
